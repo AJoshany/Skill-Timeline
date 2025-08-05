d1a83bbe58a284df86ad692e6f58f9d15d5d195f
Ali Joshany <aa6joshany@gmail.com>
2025-08-05 19:35:00 +0330

    add home route

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,12 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import About from "./pages/About/About"
 import ContactUs from "./pages/ContactUs/ContactUs"
-
+import Home from "./pages/Home/Home"
 let routes = [
   { path: "/signup", element: <AuthForm /> },
   { path: "/login", element: <AuthForm /> },
   { path: "/about", element: <About /> },
+  { path: "/", element: <Home /> },
   { path: "/contact-us", element: <ContactUs /> },
 
   {
