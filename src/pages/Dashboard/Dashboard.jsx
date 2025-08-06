cd3d21b3fdf2651dc6305c6904c9a5b5af285e6c
Ali Joshany <aa6joshany@gmail.com>
2025-08-06 08:52:16 +0330

    Add Skills Handling in Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
+import {
+  getSkills,
+  addSkill,
+  updateSkill,
+  deleteSkill,
+} from "../../services/skillsService";
 export default function Dashboard() {
   const { logout, user } = useAuth();
+  const [skills, setSkills] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchSkills() {
+      try {
+        const data = await getSkills();
+        console.log("Skills Data:", data);
+        setSkills(Array.isArray(data) ? data : []);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchSkills();
+  }, []);
+
+  const handleAdd = async () => {
+    try {
+      await addSkill({
+        title: "React Js",
+        level: 50,
+        start_date: "2024-01-05",
+        description: "Fron-end Library",
+      });
+      const updated = await getSkills();
+      setSkills(updated);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteSkill(id);
+      const updated = await getSkills();
+      setSkills(updated);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  if (loading) return <p>Lodaing...</p>;
   return (
     <>
       <div>Dashboard</div>
       <div>hello {user.email}</div>
+      <div>
+        <h1 className="mb-4 text-xl font-bold">Your Skills</h1>
+        <button
+          onClick={handleAdd}
+          className="px-4 py-2 text-white bg-blue-500 rounded"
+        >
+          Add A sample Skill
+        </button>
+
+        <ul className="mt-4">
+          {skills.map((skill) => (
+            <li
+              key={skill.id}
+              className="flex items-center justify-between py-2 border-b"
+            >
+              <span>
+                {skill.title} - {skill.level}
+              </span>
+              <button
+                onClick={() => handleDelete(skill.id)}
+                className="text-red-500 cursor-pointer"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       <button onClick={logout}>logout</button>
     </>
   );
---
+++ src/services/skillsService.js
@@ -0,0 +1,47 @@
+import { supabase } from "../supabase";
+
+export async function getSkills() {
+  const { data, error } = await supabase
+    .from("skills")
+    .select("*")
+    .order("start_date", { ascending: true });
+
+  if (error) throw error;
+  return data;
+}
+
+export async function addSkill(skill) {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) throw new Error("User not logged in");
+
+  const { data, error } = await supabase.from("skills").insert([
+    {
+      user_id: user.id,
+      ...skill,
+      level: Number(skill.level),
+    },
+  ]);
+
+  if (error) throw error;
+  return data || [];
+}
+
+export async function updateSkill(id, updates) {
+  const { data, error } = await supabase
+    .from("skills")
+    .update(updates)
+    .eq("id", 1);
+
+  if (error) throw error;
+  return data;
+}
+
+export async function deleteSkill(id) {
+  const { data, error } = await supabase.from("skills").delete().eq("id", id);
+
+  if (error) throw error;
+  return data;
+}
