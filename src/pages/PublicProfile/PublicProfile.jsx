f410c7bf703e177f3d005f4308a3d2e02d5e8211
Ali Joshany <aa6joshany@gmail.com>
2025-08-20 18:40:39 +0330

    add Style to public profile

--- src/pages/PublicProfile/PublicProfile.jsx
+++ src/pages/PublicProfile/PublicProfile.jsx
@@ -36,21 +36,29 @@ export default function PublicProfile() {
   if (!userData) return <p>User Not Found!</p>;
 
   return (
-    <div className="max-w-3xl p-4 mx-auto">
-      <h1 className="mb-4 text-3xl font-bold">{userData[0].full_name}</h1>
-      <p className="mb-6 text-gray-500">@{username}</p>
+    <div className="flex flex-col justify-between w-full md:flex-row">
+      <div>
+        <h1 className="mb-4 text-3xl font-bold">{userData[0].full_name}</h1>
+        <p className="mb-6 text-gray-500">@{username}</p>
+        <p className="mb-6 text-gray-500">{userData[0].bio}</p>
+      </div>
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-3">
         {userSkills.map((skill) => (
-          <div key={skill.id} className="p-4 border rounded shadow">
-            <h2 className="font-semibold">{skill.title}</h2>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
+          <div key={skill.id} className="flex flex-col items-center gap-3 p-4 border-2  rounded-xl shadow-[5px_5px_17px_-2px_rgba(0,_0,_0,_0.8)]">
+            <h2 className="text-xl font-semibold">{skill.title}</h2>
+            <div className="mt-2 bg-gray-200 rounded-full border-5 w-50 h-50 shadow-[inset_0px_4px_9px_1px_rgba(0,_0,_0,_0.35)] relative">
               <div
-                className="bg-blue-500 h-2.5 rounded-full"
-                style={{ width: `${skill.level}%` }}
+                className="absolute top-0 left-0 w-full h-full rounded-full"
+                style={{
+                  background: `conic-gradient(
+                  #4caf50 0% ${skill.level}%,
+                  transparent ${skill.level}% 100%
+                )` }}
               />
             </div>
             <p className="mt-1 text-sm text-gray-400">{skill.level}%</p>
+            <p className="mt-1 text-sm text-gray-300">{skill.description}</p>
           </div>
         ))}
       </div>
